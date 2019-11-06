0db07089ace1e23ba9f994a46e6b9533f92cce29
gqueiroz <gribeiro.mg@gmail.com>
2019-11-06 16:22:26 -0300

    adding package strcuture

---
+++ stac/version.py
@@ -0,0 +1,16 @@
+#
+# This file is part of Python Client Library for STAC.
+# Copyright (C) 2019 INPE.
+#
+# Python Client Library for STAC is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Version information for Python Client Library for STAC.
+
+This file is imported by ``stac.__init__``,
+and parsed by ``setup.py``.
+"""
+
+
+__version__ = '0.1.0'
