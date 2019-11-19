e30e9034c87af6bb98a413ff1f36ae55d2d0a17b
gqueiroz <gribeiro.mg@gmail.com>
2019-11-19 16:59:33 -0300

    starting first draft

---
+++ stac/stac.py
@@ -0,0 +1,29 @@
+#
+# This file is part of Python Client Library for STAC.
+# Copyright (C) 2019 INPE.
+#
+# Python Client Library for STAC is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Python API client wrapper for STAC."""
+
+class stac:
+    """This class implements a Python API client wrapper for STAC.
+
+     See https://github.com/radiantearth/stac-spec for more information on STAC.
+    """
+    def __init__(self):
+        pass
+
+    def capabilities(self):
+        pass
+
+    def conformance(self):
+        pass
+
+    def collections(self):
+        pass
+
+    def search(self):
+        pass
