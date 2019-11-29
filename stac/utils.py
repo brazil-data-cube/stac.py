2adc615c9024533e9d9fd87c6fb652eabb606d4d
Matheus C. Zaglia <mzaglia@gmail.com>
2019-11-29 14:14:50 -0200

    Adding Link Object

--- stac/utils.py
+++ stac/utils.py
@@ -7,52 +7,86 @@
 #
 """Utility data structures and algorithms."""
 
-class catalog(dict):
+
+class Link(dict):
+    """"Link object"""
+
+    def __init__(self, rel, href, type=None, title=None):
+        self['rel'] = rel
+        self['href'] = href
+        if type is not None: self['type'] = type
+        if title is not None: self['title'] = title
+        super(Link, self)
+
+    @property
+    def rel(self):
+        """Return the relation of the Link object."""
+        return self['rel']
+
+    @property
+    def href(self):
+        """Return the URL of the Link object."""
+        return self['href']
+
+    @property
+    def type(self):
+        """Return the type of the Link object."""
+        return self['type']
+
+    @property
+    def title(self):
+        """Return the title of the Link object."""
+        return self['title']
+
+
+class Catalog(dict):
     """The root STAC Catalog."""
 
-    def __init__(self, data):
+    def __init__(self, stac_version, id, description, links, **kwargs):
         """Initialize instance with dictionary data.
 
         :param data: Dict with catalog metadata.
         """
-        super(catalog, self).__init__(data or {})
+        self['stac_version'] = stac_version
+        self['id'] = id
+        self['description'] = description
+        if isinstance(links, list):
+            for link in links:
+                if not isinstance(link, Link):
+                    raise ValueError("links is not a list of Link objects.")
+            self['links'] = links
 
+        super(Catalog, self).__init__(self, **kwargs)
 
     @property
     def stac_version(self):
         """Return the STAC version."""
         return self['stac_version']
 
-
     @property
     def stac_extensions(self):
         """Return the list of supported extensions."""
         return self['stac_extensions'] if 'stac_extensions' in self else None
-
 
     @property
     def id(self):
         """Return the catalog identifier."""
         return self['id']
 
-
     @property
     def title(self):
         """Return the catalog title."""
         return self['title'] if 'title' in self else None
-
 
     @property
     def description(self):
         """Return the catalog description."""
         return self['description']
 
-
     @property
     def summaries(self):
         """Return a summary about the catalog."""
         return self['summaries'] if 'summaries' in self else None
-
 
     @property
     def links(self):
