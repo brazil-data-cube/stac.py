c4701be256880442b62ec2a6f1f5ff4a66f03afb
Matheus C. Zaglia <mzaglia@gmail.com>
2020-02-11 13:32:09 -0300

    Creating tests

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -9,31 +9,216 @@
 """Unit-test for STAC operations."""
 
 import os
+import unittest
+from unittest.mock import patch, Mock
+import stac
 
-from stac import STAC
 
 url =  os.environ.get('STAC_SERVER_URL', 'http://localhost')
 
-def test_creation():
-    service = STAC(url)
 
-    assert url.count(service.url) == 1
+class StacTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_get_patcher = patch('requests.get')
+        cls.mock_get = cls.mock_get_patcher.start()
+        cls.s = stac.STAC(url)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock_get_patcher.stop()
+
+    def test_stac(self):
+        conforms = {"conformsTo":
+            ["http://www.opengis.net/spec/wfs-1/3.0/req/core",
+            "http://www.opengis.net/spec/wfs-1/3.0/req/oas30",
+            "http://www.opengis.net/spec/wfs-1/3.0/req/html",
+            "http://www.opengis.net/spec/wfs-1/3.0/req/geojson"]
+        }
+
+        self.mock_get.return_value = Mock(status_code=200, headers={'content-type':'application/json'})
+        self.mock_get.return_value.json.return_value = conforms
+        response = self.s.conformance
+        self.assertEqual(response, conforms)
+
+        catalog = {
+            'stac_version':'0.7.0',
+            'id':'stac',
+            'links':[
+                {
+                    'href':f'{url}/collection/my_collection1',
+                    'title':'my_collection1',
+                    'rel': 'child'
+                }
+            ]
+        }
+
+        self.mock_get.return_value = Mock(status_code=200, headers={'content-type':'application/json'})
+        self.mock_get.return_value.json.return_value = catalog
+        response = self.s.catalog
+        self.assertEqual(response, set(['my_collection1']))
+
+        collections = {
+                "stac_version": "0.7.0",
+                "id": "my_collection1",
+                "title": "my_collection1",
+                "description": "my_collection1",
+                "keywords": [
+                "landsat"
+                ],
+                "extent":{
+                    "spatial": [
+                        -180,
+                        -90,
+                        180,
+                        90
+                    ],
+                "temporal":
+                    [
+                        "2013-06-01T00:00:00Z",
+                        None
+                    ]
+                },
+                "providers": [],
+                "license": "MIT",
+                "properties": {},
+                "links": [
+                    {
+                        "rel": "self",
+                        "href": f"{url}/collections/my_collection1"
+                    },
+                    {
+                        "rel": "items",
+                        "href": f"{url}/collections/my_collection1/items"
+                    }
+                ]
+            }
+
+        self.mock_get.return_value = Mock(status_code=200, headers={'content-type':'application/json'})
+        self.mock_get.return_value.json.return_value = collections
+        response = self.s.collections
+        self.assertEqual(response['my_collection1'].title, "my_collection1")
+
+        collection = stac.Collection({
+                "stac_version": "0.7.0",
+                "id": "my_collection1",
+                "title": "my_collection1",
+                "description": "my_collection1",
+                "keywords": [
+                "landsat"
+                ],
+                "extent":{
+                    "spatial": [
+                        -180,
+                        -90,
+                        180,
+                        90
+                    ],
+                "temporal":
+                    [
+                        "2013-06-01T00:00:00Z",
+                        None
+                    ]
+                },
+                "providers": [],
+                "license": "MIT",
+                "properties": {},
+                "links": [
+                    {
+                        "rel": "self",
+                        "href": f"{url}/collections/my_collection1"
+                    },
+                    {
+                        "rel": "items",
+                        "href": f"{url}/collections/my_collection1/items"
+                    }
+                ]
+            })
+        self.assertEqual(self.s.collection('my_collection1'), collection)
+
+        items = {
+            "type":"FeatureCollection",
+            "features":[
+                    {"type": "Feature",
+                    "id": "feature1",
+                    "bbox": [-122.59750209, 37.48803556, -122.2880486, 37.613537207],
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                        [[-122.308150179, 37.488035566],
+                            [-122.597502109, 37.538869539],
+                            [-122.576687533, 37.613537207],
+                            [-122.288048600, 37.562818007],
+                            [-122.308150179, 37.488035566]]]
+                    },
+                    "properties": {
+                        "datetime": "2016-05-03T13:21:30.040Z"
+                    },
+                    "collection": "my_collection1",
+                    "links": [
+                        {
+                        "rel": "self",
+                        "href":  f"{url}/collections/my_collection1/feature1"
+                        },
+                        {
+                        "rel": "collection",
+                        "href": f"{url}/collections/my_collection1"
+                        }
+                    ],
+                    "assets": {
+                        "thumbnail": {
+                        "href": f"{url}/collections/my_collection1/feature1/thumbnail.png",
+                        "title": "Thumbnail"
+                        }
+                    }
+                },
+                {"type": "Feature",
+                    "id": "feature2",
+                    "bbox": [-122.59750209, 37.48803556, -122.2880486, 37.613537207],
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                        [
+                            [-122.308150179, 37.488035566],
+                            [-122.597502109, 37.538869539],
+                            [-122.576687533, 37.613537207],
+                            [-122.288048600, 37.562818007],
+                            [-122.308150179, 37.488035566]
+                        ]
+                        ]
+                    },
+                    "properties": {
+                        "datetime": "2016-05-10T13:21:30.040Z"
+                    },
+                    "collection": "my_collection1",
+                    "links": [
+                        {
+                        "rel": "self",
+                        "href": f"{url}/collections/my_collection1/feature2"
+                        },
+                        {
+                        "rel": "collection",
+                        "href": f"{url}/collections/my_collection1"
+                        }
+                    ],
+                    "assets": {
+                        "thumbnail": {
+                        "href": f"{url}/collections/my_collection1/feature1/thumbnail.png",
+                        "title": "Thumbnail"
+                        }
+                    }
+                }
+            ]
+        }
+
+        self.mock_get.return_value = Mock(status_code=200, headers={'content-type':'application/json'})
+        self.mock_get.return_value.json.return_value = items
+
+        response = self.s.collection('my_collection1').get_items()
+
+        self.assertEqual(response.features[0].id, 'feature1')
 
 
-def test_conformance():
-    service = STAC(url)
 
-    retval = service.conformance()
-
-    assert 'conformsTo' in retval
-
-
-def test_catalog():
-    service = STAC(url)
-
-    retval = service.catalog()
-
-    common_keys = { 'stac_version', 'id', 'description', 'links' }
-
-    assert  common_keys <= set(retval.keys())
-
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
