d1542cfbee606e04636b56fe13f8b26500a54cd3
Matheus C. Zaglia <mzaglia@gmail.com>
2020-03-05 08:21:07 -0300

    Changing example url for STAC 0.8.0

--- examples/ex-01.py
+++ examples/ex-01.py
@@ -8,66 +8,45 @@ import stac
 from matplotlib import pyplot
 
 #%%
-
-s = stac.STAC('http://brazildatacube.dpi.inpe.br/bdc-stac/0.7.0')
+s = stac.STAC('http://brazildatacube.dpi.inpe.br/bdc-stac/0.8.0', True)
 
 
 #%%
-
-
 s.catalog
 
-
 #%%
-
-
 collection = s.collections['C64mMEDIAN']
 collection
 
-
 #%%
-
 items = s.collections['C64mMEDIAN'].get_items()
 items
 
 #%%
-
-
 items.features[0].assets
 
 #%%
-
 red = items.features[0].assets['red'].download()
 green = items.features[0].assets['green'].download()
 blue = items.features[0].assets['blue'].download()
 
 red
 
-
 #%%
-
-
 r = rasterio.open(red).read(1)
 g = rasterio.open(green).read(1)
 b = rasterio.open(blue).read(1)
 
 
 #%%
-
-
 r.max()
 
 #%%
-
-
 def normalize(array):
     """Normalizes numpy arrays into scale 0.0 - 1.0"""
     array_min, array_max = array.min(), array.max()
     return ((array - array_min)/(array_max - array_min))
 
-
 #%%
-
-
 rgb = np.dstack((normalize(r), normalize(g), normalize(b)))
 pyplot.imshow(rgb)
